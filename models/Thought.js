a9b7e1578b19438b339019d10e9e0066b86cf805
BLealan <143086275+BLealan@users.noreply.github.com>
2024-02-14 16:38:34 +0000

    added Thought Model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const thoughtSchema = mongoose.Schema({
     thoughtText: {
@@ -24,11 +24,47 @@ const thoughtSchema = mongoose.Schema({
         type: String,
         required: true,
     },
-    reactions: {
-        //Array of nested documents created with the `reactionSchema`
+    reactions: [reactionSchema],
+},
+{
+    toJSON: {
+        virtuals: true,
     },
+    id: false,
 });
 
+const rectionSchema = mongoose.Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        //default value set to new ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(value){
+                return /^.{1,280}$/.test(value);
+            },
+        },
+    },
+    username: {
+        type: String,
+        required: true 
+    },
+    createdAt: {
+        type: Date ,
+        default: Date.now,
+        get: (date) => {
+            if (date) return date.toISOString().split("T") [0];
+        },
+        //Set default value to the current timestamp
+        //Use a getter method to format the timestamp on query
+    },
+    
+})
 //Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+thoughtSchema.virtual('reactionCount').get(function () {return this.reactions.length;});
+
+const Thoughts = model('thougt', thoughtSchema);
 
 module.exports = Thoughts;
